727185aa3d492d30c69c127444d8733e949cf481
Yashu Mittal <mittalyashu77@gmail.com>
2021-05-08 20:56:29 +0530

    format code using eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
-	root: true,
-	env: {
-		browser: true,
-		node: true,
-		es6: true
-	},
-	plugins: ["vue"],
-	extends: ["eslint:recommended", "plugin:vue/recommended"],
-	rules: {
-		"semi": [1, "always"],
-		"vue/html-indent": [1, 2],
-		"indent": [1, 2],
-		"comma-dangle": [1, "never"],
-		"vue/comma-dangle": [1, "never"],
-		"vue/max-attributes-per-line": [
-			2,
-			{
-				singleline: 2,
-			}
-		]
-	},
-	overrides: [
-		{
-			files: ["**/*.spec.js"],
-			env: {
-				jest: true
-			},
-			plugins: ["jest"]
-		}
-	]
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  plugins: ["vue"],
+  extends: ["eslint:recommended", "plugin:vue/recommended"],
+  rules: {
+    "semi": [1, "always"],
+    "vue/html-indent": [1, 2],
+    "indent": [1, 2],
+    "comma-dangle": [1, "never"],
+    "vue/comma-dangle": [1, "never"],
+    "vue/max-attributes-per-line": [
+      2,
+      {
+        singleline: 2
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: ["**/*.spec.js"],
+      env: {
+        jest: true
+      },
+      plugins: ["jest"]
+    }
+  ]
 };
